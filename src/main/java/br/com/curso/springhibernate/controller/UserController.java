004f0588bf1a57cd2239f3b7da96a1ac291f079b
Erick E. Tavares <erick-tavares@hotmail.com>
2021-01-14 17:54:06 -0300

    create UserService, implements Logger slf4j

---
+++ src/main/java/br/com/curso/springhibernate/controller/UserController.java
@@ -0,0 +1,34 @@
+package br.com.curso.springhibernate.controller;
+
+import br.com.curso.springhibernate.entities.User;
+import br.com.curso.springhibernate.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+}
--- src/main/java/br/com/curso/springhibernate/repositories/UserRepository.java
+++ src/main/java/br/com/curso/springhibernate/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package br.com.curso.springhibernate.repositories;
 
 import br.com.curso.springhibernate.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
